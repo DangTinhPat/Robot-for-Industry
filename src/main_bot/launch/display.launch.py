9e13e2a759128bc994a94302a40df9c8b3ddcf39
dvt <dangtinh.ftcpy@gmail.com>
2026-06-17 13:29:55 +0700

    air_1

--- src/main_bot/launch/display.launch.py
+++ src/main_bot/launch/display.launch.py
@@ -1,42 +1,13 @@
-import os
-import xacro
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg_path = get_package_share_directory('main_bot')
-    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
-    robot_description = xacro.process_file(xacro_file).toxml()
-
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='True',
-            description='true khi chạy cùng Gazebo, false khi standalone'
-        ),
         Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{'robot_description': robot_description, 'use_sim_time': use_sim_time}],
-            output='screen'
-        ),
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            parameters=[{'robot_description': robot_description, 'use_sim_time': use_sim_time}],
-            output='screen'
-        ),
-        Node(
-            
             package='rviz2',
             executable='rviz2',
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'
+            parameters=[{'use_sim_time': True}],
+            output='screen',
         ),
     ])
